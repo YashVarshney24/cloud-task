eecf6083861204713eb6031cd855794b8eb7a214
YashVarshney24 <46783662+YashVarshney24@users.noreply.github.com>
2020-06-17 14:54:32 +0530

    df

--- index.php
+++
@@ -1,9 +0,0 @@
-<html><head><title><?php echo "Yash Varshney";?></title></head>
-<body
-<?php 
-$image_url='https://www.google.com/intl/en_com/images/srpr/logo3w.png';
-?>
-
-<img src="<?php echo $image_url;?>">
-
-<br><h1>I am Yash Varshney</h1>
